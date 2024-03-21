2a7320bf7f5cdfc7e61ef28ae91c7ce1b960f51c
vvagraphics <vvagraphics@gmail.com>
2024-03-21 14:41:52 -0400

    day3_update

--- main.py
+++ main.py
@@ -407,30 +407,78 @@
 
 
 # Love Calc Exer
-print("The Love Calculator is calculating your score...")
-name1 = "Angela Yu" # What is your name?
-name2 = "Jack Bauer" # What is their name?
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
-combo = name1 + name2
-combo_lower = combo.lower()
-t = combo_lower.count("t")
-r = combo_lower.count("r")
-u = combo_lower.count("u")
-e = combo_lower.count("e")
+# print("The Love Calculator is calculating your score...")
+# name1 = "Angela Yu" # What is your name?
+# name2 = "Jack Bauer" # What is their name?
+# # 🚨 Don't change the code above 👆
+# # Write your code below this line 👇
+# combo = name1 + name2
+# combo_lower = combo.lower()
+# t = combo_lower.count("t")
+# r = combo_lower.count("r")
+# u = combo_lower.count("u")
+# e = combo_lower.count("e")
 
-l = combo_lower.count("l")
-o = combo_lower.count("o")
-v = combo_lower.count("v")
-e = combo_lower.count("e")
+# l = combo_lower.count("l")
+# o = combo_lower.count("o")
+# v = combo_lower.count("v")
+# e = combo_lower.count("e")
 
-first_digit = t + r + u + e
-last_digit = l + o + v + e
+# first_digit = t + r + u + e
+# last_digit = l + o + v + e
 
-score = int(str(first_digit) + str(last_digit))
-if (score < 10) or (score > 90):
-  print(f"Your score is {score}, you go together like coke and mentos.")
-elif (score >= 40) and (score <= 50):
-  print(f"Your score is {score}, you are alright together.")
-else:
-  print(f"Your score is {score}.")
+# score = int(str(first_digit) + str(last_digit))
+# if (score < 10) or (score > 90):
+#   print(f"Your score is {score}, you go together like coke and mentos.")
+# elif (score >= 40) and (score <= 50):
+#   print(f"Your score is {score}, you are alright together.")
+# else:
+#   print(f"Your score is {score}.")
+
+
+# print('''
+# *******************************************************************************
+#           |                   |                  |                     |
+#  _________|________________.=""_;=.______________|_____________________|_______
+# |                   |  ,-"_,=""     `"=.|                  |
+# |___________________|__"=._o`"-._        `"=.______________|___________________
+#           |                `"=._o`"=._      _`"=._                     |
+#  _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+# |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+# |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+#           |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+#  _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+# |                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+# |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+# ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+# /______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+# ____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+# /______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+# ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+# /______/______/______/______/______/______/______/______/______/______/_____ /
+# *******************************************************************************
+# ''')
+# print("Welcome to Treasure Island.")
+# print("Your mission is to find the treasure.") 
+
+# #https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+# #Write your code below this line 👇
+# choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+# if choice1 == "left":
+#   choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+#   if choice2 == "wait":
+#     choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+#     if choice3 == "red":
+#       print("It's a room full of fire. Game Over.")
+#     elif choice3 == "yellow":
+#       print("You found the treasure! You Win!")
+#     elif choice3 == "blue":
+#       print("You enter a room of beasts. Game Over.")
+#     else:
+#       print("You chose a door that doesn't exist. Game Over.")
+#   else:
+#     print("You get attacked by an angry trout. Game Over.")
+# else:
+#   print("You fell into a hole. Game Over.")
+
