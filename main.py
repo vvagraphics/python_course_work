3ba300c431e95a06057dbc38afa548ab5020e628
vvagraphics <vvagraphics@gmail.com>
2024-03-31 10:19:01 -0400

    day9

--- main.py
+++ main.py
@@ -1341,3 +1341,7 @@
 # Day 9
 # Python Dictionary
 programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
