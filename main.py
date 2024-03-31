88dc966aceabc2423b9b8b73bbb5157a77be4280
vvagraphics <vvagraphics@gmail.com>
2024-03-31 12:38:40 -0400

    day9_final

--- main.py
+++ main.py
@@ -1340,8 +1340,237 @@
 
 # Day 9
 # Python Dictionary
-programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
+# programming_dictionary = {"Bug": "An error in a program that prevents the program from running as expected.", "Function": "A piece of code that you can easily call over and over again."}
 
-for key in programming_dictionary:
-    print(key)
-    print(programming_dictionary[key])
+# for key in programming_dictionary:
+#     print(key)
+#     print(programming_dictionary[key])
+# student_scores = {
+#   "Harry": 81,
+#   "Ron": 78,
+#   "Hermione": 99, 
+#   "Draco": 74,
+#   "Neville": 62,
+# }
+
+# for key in student_scores:
+#     if student_scores[key] <= 70:
+#         student_scores[key] = "Fail"
+#     elif student_scores[key] <=80:
+#         student_scores[key] = "Acceptable"
+#     elif student_scores[key] <=90:
+#         student_scores[key] = "Exceeds Expectations"
+#     else:
+#         student_scores[key] > 90
+#         student_scores[key] = "Outstanding"
+
+# print(student_scores)
+
+
+# student_scores = {
+#   "Harry": 81,
+#   "Ron": 78,
+#   "Hermione": 99, 
+#   "Draco": 74,
+#   "Neville": 62,
+# }
+# # 🚨 Don't change the code above 👆
+# # TODO-1: Create an empty dictionary called student_grades.
+# student_grades = {}
+
+# # TODO-2: Write your code below to add the grades to student_grades.👇
+
+# student_grades = student_scores
+# for key in student_scores:
+    
+#     if student_scores[key] <= 70:
+#         student_scores[key] = "Fail"
+#     elif student_scores[key] <=80:
+#         student_scores[key] = "Acceptable"
+#     elif student_scores[key] <=90:
+#         student_scores[key] = "Exceeds Expectations"
+#     else:
+#         student_scores[key] > 90
+#         student_scores[key] = "Outstanding"
+
+# # 🚨 Don't change the code below 👇
+# print(student_grades)
+
+# solution
+
+# student_scores = {
+#   "Harry": 81,
+#   "Ron": 78,
+#   "Hermione": 99, 
+#   "Draco": 74,
+#   "Neville": 62,
+# }
+
+# # TODO-1: Create an empty dictionary called student_grades.
+# student_grades = {}
+
+# # TODO-2: Covert scores into grades.
+# for student in student_scores:
+#   score = student_scores[student]
+#   if score > 90:
+#     student_grades[student] = "Outstanding"
+#   elif score > 80:
+#     student_grades[student] = "Exceeds Expectations"
+#   elif score > 70:
+#     student_grades[student] = "Acceptable"
+#   else:
+#     student_grades[student] = "Fail"
+    
+# print(student_grades)
+
+# travel_log = {
+#     "France": {"cities_visited": ["Paris", "Lille", "Dijon"]},
+#     "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+# }
+# ##Python Dictionaries
+
+# programming_dictionary = {
+#   "Bug": "An error in a program that prevents the program from running as expected.", 
+#   "Function": "A piece of code that you can easily call over and over again.",
+# }
+
+# #Retrieving items from dictionary.
+# # print(programming_dictionary["Function"])
+
+# #Adding new items to dictionary.
+# programming_dictionary["Loop"] = "The action of doing something over and over again."
+
+# #Create an empty dictionary.
+# empty_dictionary = {}
+
+# #Wipe an existing dictionary
+# # programming_dictionary = {}
+# # print(programming_dictionary)
+
+# #Edit an item in a dictionary
+# programming_dictionary["Bug"] = "A moth in your computer."
+# # print(programming_dictionary)
+
+# #Loop through a dictionary
+# # for key in programming_dictionary:
+# #   print(key)
+# #   print(programming_dictionary[key])
+
+# #######################################
+
+# #Nesting 
+# capitals = {
+#   "France": "Paris",
+#   "Germany": "Berlin",
+# }
+
+# #Nesting a List in a Dictionary
+
+# travel_log = {
+#   "France": ["Paris", "Lille", "Dijon"],
+#   "Germany": ["Berlin", "Hamburg", "Stuttgart"],
+# }
+
+# #Nesting Dictionary in a Dictionary
+
+# travel_log = {
+#   "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+#   "Germany": {"cities_visited": ["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+# }
+
+# #Nesting Dictionaries in Lists
+
+# travel_log = [
+# {
+#   "country": "France", 
+#   "cities_visited": ["Paris", "Lille", "Dijon"], 
+#   "total_visits": 12,
+# },
+# {
+#   "country": "Germany",
+#   "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+#   "total_visits": 5,
+# },
+# ]
+
+# country = input() # Add country name
+# visits = int(input()) # Number of visits
+# list_of_cities = eval(input()) # create list from formatted string
+
+# travel_log = [
+#   {
+#     "country": "France",
+#     "visits": 12,
+#     "cities": ["Paris", "Lille", "Dijon"]
+#   },
+#   {
+#     "country": "Germany",
+#     "visits": 5,
+#     "cities": ["Berlin", "Hamburg", "Stuttgart"]
+#   },
+# ]
+# # Do NOT change the code above 👆
+
+# # TODO: Write the function that will allow new countries
+# # to be added to the travel_log. 
+# def add_new_country(country, visits, list_of_cities):
+#   new_country = {}
+#   new_country["country"] = country
+#   new_country["visits"] = visits
+#   new_country["cities"] = list_of_cities
+#   travel_log.append(new_country)
+  
+  
+
+
+# # Do not change the code below 👇
+# add_new_country(country, visits, list_of_cities)
+# print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+# print(f"My favourite city was {travel_log[2]['cities'][0]}.")
+
+# Final
+
+# from replit import clear
+#HINT: You can call clear() to clear the output in the console.
+# from art_auction import logo
+# print(logo)
+# bids = {"name":5}
+
+
+# names = input("What is your name?: ")
+# bids = input("What is your bid?: $")
+# any_other = input("Are there any other bidders? Type 'yes' or 'no'")
+
+# def highest_bidder():
+
+# solution
+# from art_auction import logo
+# print(logo)
+
+# bids = {}
+
+# bidding_finished = False
+
+# def find_highest_bidder(bidding_record):
+#     highest_bid = 0
+#     winner = ""
+#     for bidder in bidding_record:
+#         bid_amount = bidding_record[bidder]
+#         if bid_amount > highest_bid:
+#             highest_bid = bid_amount
+#             winner = bidder
+#     print(f"the winner is {winner} with bid of {highest_bid}")
+
+# while not bidding_finished:
+#     name = input("What is your name?: ")
+#     price = int(input("What is your bid?: $"))
+#     bids[name] = price
+
+#     should_continue = input("Are there any other bidders? Type 'yes' or 'no'")
+#     if should_continue == "no":
+#         bidding_finished = True
+#         find_highest_bidder(bids)
+
+#     elif should_continue == "yes":
+#         # clear()
+#         print("\033[H\033[J")
